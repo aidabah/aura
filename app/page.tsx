8cc019ab89cda8d74c57f8d82b4f75214f640282
Aida Bah <aida.bah@slu.edu>
2025-11-27 22:07:41 -0600

    Initial environment setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center p-10">
+      <h1 className="text-4xl font-bold">AURA</h1>
+      <p className="mt-4">Paste your Pinterest board.</p>
+    </main>
+  );
+}
+
